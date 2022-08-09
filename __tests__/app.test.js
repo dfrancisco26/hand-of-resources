ae68498a48fd15597ba09dc58e12586e0a17bd07
David Francisco <dfrancisco2677@gmail.com>
2022-08-09 07:26:55 -0700

    Wrote first test and table.

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,15 +1,26 @@
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
-// const request = require('supertest');
-// const app = require('../lib/app');
+const request = require('supertest');
+const app = require('../lib/app');
 
 describe('backend-express-template routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+
+  it('#GET /newgames should return list of newgames', async () => {
+    const res = await request(app).get('/newgames');
+    expect(res.status).toEqual(200);
+    expect(res.body).toEqual([
+      { id: '1', name: 'Stray', genre: 'Cat Simulator', release: 2022 },
+      { id: '2', name: 'Final Fantasy VII: Remake Intergrade', genre: 'RPG', release: 2021 },
+      { id: '3', name: 'Hades', genre: 'Action Roguelite', release: 2018 },
+      { id: '4', name: 'Vampire Survivors', genre: 'Action/Survival', release: 2021 }
+
+    ]);
   });
+
+
   afterAll(() => {
     pool.end();
   });
--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,17 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+DROP table IF EXISTS newgames;
+
+CREATE TABLE newgames (
+    id BIGINT GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR,
+    genre VARCHAR,
+    release SMALLINT
+    );
+
+INSERT INTO newgames (name, genre, release)
+VALUES
+('Stray', 'Cat Simulator', 2022),
+('Final Fantasy VII: Remake Intergrade', 'RPG', 2021),
+('Hades', 'Action Roguelite', 2018),
+('Vampire Survivors', 'Action/Survival', 2021);
