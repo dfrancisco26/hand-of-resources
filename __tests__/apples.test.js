fd5b217903586094fa508abd631903cdd51ff0a3
David Francisco <dfrancisco2677@gmail.com>
2022-08-15 11:39:44 -0700

    POST route functions properly. I might have also made the getId without committing.

--- __tests__/apples.test.js
+++ __tests__/apples.test.js
@@ -17,6 +17,31 @@ describe('apples routes', () => {
       { id: '3', name: 'Granny Smith', color: 'Green', feature: 'Tartness' }
     ]);
   });
+
+  it('#GET apples/:id should return specific apple', async () => {
+    const res = await request(app).get('/apples/1');
+    expect(res.status).toBe(200);
+    expect(res.body).toEqual({
+      id: '1',
+      name: 'Honeycrisp',
+      color: 'yellow-red',
+      feature: 'Crunch'
+    });
+  });
+  
+  it('#POST /apples creates a new apple', async () => {
+    const Apple = {
+      'name': 'Gala',
+      'color': 'red-yellow',
+      'feature': 'shelf life'
+    };
+    const res = await request(app).post('/apples').send(Apple);
+    expect(res.status).toBe(200);
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      ...Apple,
+    });
+  });
   
   afterAll(() => {
     pool.end();
