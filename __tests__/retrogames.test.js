8c50980d5e3b4c89085cc5ec9f27028a6d6dc36b
David Francisco <dfrancisco2677@gmail.com>
2022-08-14 17:02:30 -0700

    Second table created. Test runs for getting all retrogames.

---
+++ __tests__/retrogames.test.js
@@ -0,0 +1,27 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+
+describe('backend-express-template routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+
+  it('#GET /retrogames should return list of retrogames', async () => {
+    const res = await request(app).get('/retrogames');
+    expect(res.status === 200);
+    expect(res.body).toEqual([
+      { id: '1', name: 'Legend of Dragoon', genre: 'JRPG', release: 1999 },
+      { id: '2', name: 'Pokémon Red', genre: 'Adventure/JRPG', release: 1996 },
+      { id: '3', name: 'Syphon Filter', genre: 'Stealth Shooter', release: 1999 },
+      { id: '4', name: 'Gran Turismo 2', genre: 'Racing Simulator', release: 1999 }
+    ]);
+  });
+
+});
+
+
+afterAll(() => {
+  pool.end();
+});
--- sql/setup.sql
+++ sql/setup.sql
@@ -1,6 +1,7 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
 DROP table IF EXISTS newgames;
+DROP table IF EXISTS retrogames;
 
 CREATE TABLE newgames (
     id BIGINT GENERATED ALWAYS AS IDENTITY,
@@ -15,3 +16,17 @@ VALUES
 ('Final Fantasy VII: Remake Intergrade', 'RPG', 2021),
 ('Hades', 'Action Roguelite', 2018),
 ('Vampire Survivors', 'Action/Survival', 2021);
+
+CREATE TABLE retrogames (
+    id BIGINT GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR,
+    genre VARCHAR,
+    release SMALLINT
+    );
+
+INSERT INTO retrogames (name, genre, release)
+VALUES
+('Legend of Dragoon', 'JRPG', 1999),
+('Pokémon Red', 'Adventure/JRPG', 1996),
+('Syphon Filter', 'Stealth Shooter', 1999),
+('Gran Turismo 2', 'Racing Simulator', 1999);
